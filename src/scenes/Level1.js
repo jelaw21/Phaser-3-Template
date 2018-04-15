7d4fb162601933f68739bf31739495fc5583d505
jelaw21 <jelaw21@gmail.com>
2018-04-15 17:01:28 -0400

    Worked on World view

--- src/scenes/Level1.js
+++ src/scenes/Level1.js
@@ -6,18 +6,19 @@ export default class Level1 extends Phaser.Scene {
     }
 
     create(){
+
         //CREATE THE MAP
         this.map = this.make.tilemap({key: 'forest'});
         this.tiles = this.map.addTilesetImage('backgroundTiles1', 'backgroundTiles1');
-        var tiles2 = this.map.addTilesetImage('backgroundTiles2', 'backgroundTiles2');
-        var tiles3 = this.map.addTilesetImage('ProjectUtumno_full', 'itemTiles');
-        var tiles4 = this.map.addTilesetImage('townTiles1', 'townTiles1');
-        this.layer1 = this.map.createDynamicLayer('grassLayer', this.tiles, 0, 0);
-        this.map.createDynamicLayer('grassLayer', tiles2, 0,0);
-        //this.map.createStaticLayer('groundCover', tiles, 0, 0);
-        this.map.createDynamicLayer('groundCover', tiles2, 0, 0);
-        this.blocked = this.map.createDynamicLayer('blockedLayer', this.tiles, 0,0);
-        //this.blocked2 = this.map.createStaticLayer('blockedLayer', tiles2, 0,0);
+        this.tiles2 = this.map.addTilesetImage('backgroundTiles2', 'backgroundTiles2');
+        //var tiles3 = this.map.addTilesetImage('ProjectUtumno_full', 'itemTiles');
+        //var tiles4 = this.map.addTilesetImage('townTiles1', 'townTiles1');
+        this.map.createDynamicLayer('grassLayer', this.tiles, 0, 0);
+        //this.map.createDynamicLayer('grassLayer', this.tiles2, 0,0);
+        this.map.createStaticLayer('groundCover', this.tiles, 0, 0);
+        //this.map.createDynamicLayer('groundCover', this.tiles2, 0, 0);
+        this.blocked = this.map.createDynamicLayer('blockedLayer', this.tiles);
+        this.blocked2 = this.map.createStaticLayer('blockedLayer', this.tiles2);
 
 
         //HAD TO CREATE THE PLAYER TO PUT FOREGROUND ON TOP
@@ -25,7 +26,7 @@ export default class Level1 extends Phaser.Scene {
 
         //PART OF MAP PLAYER WALKS BEHIND
         this.map.createStaticLayer('foregroundLayer', this.tiles, 0, 0);
-        this.map.createDynamicLayer('foregroundLayer', tiles2, 0,0);
+        //this.map.createDynamicLayer('foregroundLayer', this.tiles2, 0,0);
 
         //PLAYER OPTIONS - NOT SURE HOW I NEED TO DO THIS PART
         this.player.setCollideWorldBounds(true);
@@ -39,7 +40,7 @@ export default class Level1 extends Phaser.Scene {
         //CAMERA CRAP THAT I'M NOT SURE HOW IT WORKS
         var cam = this.cameras.main;
         cam.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
-        cam.zoom = 2;
+        cam.zoom = 1.5;
         cam.startFollow(this.player);
         cam.scrollX = 2;
 
@@ -80,6 +81,11 @@ export default class Level1 extends Phaser.Scene {
             frameRate: 10,
             repeat: -1
         });
+        this.standStill = this.anims.create({
+            key:'stopped',
+            frames: [{key: 'playerS', frame: 0}],
+            frameRate: 10
+        });
 
        // var graphicsMap = this.add.graphics();
         //this.blocked.renderDebug(graphicsMap);
@@ -87,46 +93,37 @@ export default class Level1 extends Phaser.Scene {
        }
 
     update(){
-        //bump
 
-
-       // var graphicsMap = this.add.graphics();
+        // var graphicsMap = this.add.graphics();
         //this.player.body.drawDebug(graphicsMap);
 
         if((this.cursors.up.isDown || this.cursors.down.isDown) && this.cursors.left.isDown){
-
-            //this.playAnim(this.player, 'left');
-            //this.resumeAnim(this.animLeft);
+            this.playAnim(this.player, 'left');
             if(this.cursors.up.isDown)
                 this.player.body.setVelocity(-100, -100);
             else
                 this.player.body.setVelocity(-100, 100);
         }else if((this.cursors.up.isDown || this.cursors.down.isDown) && this.cursors.right.isDown){
-            //this.playAnim(this.player, 'right');
-            //this.resumeAnim(this.animRight);
+            this.playAnim(this.player, 'right');
             if(this.cursors.up.isDown)
                 this.player.body.setVelocity(100, -100);
-
             else
                 this.player.body.setVelocity(100, 100);
         }else if(this.cursors.right.isDown){
             this.player.body.setVelocity(100, 0);
             this.playAnim(this.player, 'right');
-            //this.resumeAnim(this.animRight);
         }else if(this.cursors.left.isDown){
             this.player.body.setVelocity(-100, 0);
-            //this.playAnim(this.player, 'left');
-            //this.resumeAnim(this.animLeft)
+            this.playAnim(this.player, 'left');
         }else if(this.cursors.up.isDown){
            this.player.body.setVelocity(0, -100);
-            //this.playAnim(this.player, 'up');
-            //this.resumeAnim(this.animUp)
+            this.playAnim(this.player, 'up');
         }else if(this.cursors.down.isDown){
             this.player.body.setVelocity(0, 100);
-            //this.playAnim(this.player, 'down');
-            //this.resumeAnim(this.animDown)
+            this.playAnim(this.player, 'down');
         }else{
             this.player.body.setVelocity(0,0);
+            this.playAnim(this.player, 'stopped');
         }
     }
 
